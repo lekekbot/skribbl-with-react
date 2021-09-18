9fa155a42914e024479bd7e56821a7d94c7ac952
RandomGeeb <81279561+RandomGeeb@users.noreply.github.com>
2021-09-18 19:24:26 +0800

    changed canvas to be cooler

--- frontend/src/Components/Canvas/Canvas.js
+++ frontend/src/Components/Canvas/Canvas.js
@@ -1,14 +1,22 @@
 import React, { useState, useRef, useEffect, useContext } from 'react'
+import { Col, Row, Container } from 'react-bootstrap'
+
+//styles
+import styles from './Canvas.module.css'
 
 export default function Canvas(props) {
 
     const canvasRef = useRef(null)
 
     useEffect(() => {
-        const canvas = canvasRef.current
-        const ctx = canvas.getContext('2d')
 
-        //Canvas
+        //canvas settings
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        canvas.height = 500;
+        canvas.width = 1000;
+
+        //canvas functions
         let drawing = false;
 
         function startPosition(e) {
@@ -26,19 +34,25 @@ export default function Canvas(props) {
             ctx.lineWidth = 10;
             ctx.lineCap = "round";
 
-            ctx.lineTo(e.clientX, e.clientY);
+            ctx.lineTo(e.offsetX, e.offsetY);
             ctx.stroke();
             ctx.beginPath();
-            ctx.moveTo(e.clientX, e.clientY);
-
+            ctx.moveTo(e.offsetX, e.offsetY);
+            
         }
 
+        //my cool event listeners
         canvas.addEventListener("mousedown", startPosition);
         canvas.addEventListener("mouseup", endPosition);
         canvas.addEventListener("mousemove", draw);
 
     }, [])
 
-    return <canvas ref={canvasRef} {...props} />
+    return (
+        <Row className={styles.canvasBox}>
+            <canvas className={styles.canvas} ref={canvasRef} {...props} />
+            {/*Ill add options for the canvas brush some other time*/}
+        </Row>
+    )
 
 }
--- frontend/src/Pages/CanvasTest/CanvasTest.js
+++ frontend/src/Pages/CanvasTest/CanvasTest.js
@@ -1,7 +1,20 @@
-import React, {useState, useRef, useEffect, useContext} from 'react'
+import React, { useState, useRef, useEffect, useContext } from 'react'
+import { Col, Row, Container } from 'react-bootstrap'
+
+//import Canvas Component
 import Canvas from '../../Components/Canvas/Canvas'
 
 export default function CanvasTest() {
 
-      return <Canvas/>
+    return (
+        <Container>
+            <Row style={{height: 100}}>
+
+            </Row>
+            <Row>
+                <Canvas />
+            </Row>
+        </Container>
+
+    )
 }
